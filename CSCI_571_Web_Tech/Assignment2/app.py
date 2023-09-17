c326dbb72f7910b7f031d93ee922c4e807ddd02b
sakethanne <36626206+Sakethanne@users.noreply.github.com>
2023-09-16 21:53:23 -0700

    got the results for ebay api

--- CSCI_571_Web_Tech/Assignment2/app.py
+++ CSCI_571_Web_Tech/Assignment2/app.py
@@ -1,16 +1,81 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
+import requests
+import json
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    print(my_flask_function())
-    return "Hello, Flask!"
+def index():
+    return render_template('ebay.html')
 
-if __name__ == '__main__':
-    # Change the port to your desired port number (e.g., 8080)
-    app.run(debug=True, port=5500)
+@app.route('/sendinputdata', methods=['POST'])
+def processinputdata():
+    data = request.json  # Get the JSON data from the request
+    i = 2
+    condtapi = ''
+    try:
+        api = 'https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsAdvanced&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=SaiVenka-WebAppli-PRD-672a069ab-61e3ce4f&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords='
+        api += data['keyword']
+        api += '&paginationInput.entriesPerPage=10&sortOrder='
+        api += data['sortby']
+        api += '&itemFilter(0).name=MinPrice&itemFilter(0).value='
+        api += data['pricefrom']
+        api += '&itemFilter(0).paramName=Currency&itemFilter(0).paramValue=USD'
+        api += '&itemFilter(1).name=MaxPrice&itemFilter(1).value='
+        api += data['priceto']
+        api += '&itemFilter(1).paramName=Currency&itemFilter(1).paramValue=USD'
+        
+        if(data['newvalue'] == True):    
+            condtapi += '&itemFilter('+str(i)+').name=Condition&itemFilter('+str(i)+').value='
+            condtapi += str(1000)
+        
+        if(data['usedvalue'] == True):    
+            condtapi += '&itemFilter('+str(i)+').name=Condition&itemFilter('+str(i)+').value='
+            condtapi += str(3000)
 
+        if(data['vergoodvalue'] == True):    
+            condtapi += '&itemFilter('+str(i)+').name=Condition&itemFilter('+str(i)+').value='
+            condtapi += str(4000)
 
-@app.route('/my-flask-function', methods=['GET'])
-def my_flask_function():
-    print('this is a testing')
+        if(data['goodvalue'] == True):    
+            condtapi += '&itemFilter('+str(i)+').name=Condition&itemFilter('+str(i)+').value='
+            condtapi += str(5000)
+
+        if(data['acceptablevalue'] == True):    
+            condtapi += '&itemFilter('+str(i)+').name=Condition&itemFilter('+str(i)+').value='
+            condtapi += str(6000)
+        
+        i = i + 1
+
+        api += condtapi
+
+        if(data['returnacceptedvalue'] == True):    
+            api += '&itemFilter('+str(i)+').name=ReturnsAcceptedOnly&itemFilter('+str(i)+').value='
+            api += str(data['returnacceptedvalue']).lower()
+            i = i + 1
+
+        if(data['freevalue'] == True):    
+            api += '&itemFilter('+str(i)+').name=FreeShippingOnly&itemFilter('+str(i)+').value='
+            api += str(data['freevalue']).lower()
+            i = i + 1
+
+        if(data['expedictedvalue'] == True):    
+            api += '&itemFilter('+str(i)+').name=ExpeditedShippingType&itemFilter('+str(i)+').value='
+            api += 'Expedited'
+            i = i + 1
+
+        print(api)
+
+        response = requests.get(api)
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            data = response.json()  # Parse the JSON response
+            print("Data received:")
+        else:
+            print(f"Error: Status code {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+    result = {
+        'message': 'Data received successfully from api',
+        'data': data,
+    }
+    return jsonify(result)
--- CSCI_571_Web_Tech/Assignment2/static/js/ebay.js
+++ CSCI_571_Web_Tech/Assignment2/static/js/ebay.js
@@ -12,16 +12,23 @@ function cleanform(event) {
     document.getElementById('returnaccepted').checked = false;
     document.getElementById('free').checked = false;
     document.getElementById('expedicted').checked = false;
+    cleanresults(event);
   }
-  
+
+  function cleanresults(event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+    document.getElementById('total-results').innerHTML = '';
+    document.getElementById('items-container').innerHTML = '';
+  }
+
   function submitdata(event) {
+    cleanresults(event);
     event.preventDefault(); // Prevent the default form submission behavior
     if (validatedata(event) === false){
         console.log('Please enter correct input values');
     }
     else{
         console.log('continue');
-        callFlaskFunction();
         const keywords = document.getElementById('keywords').value;
         const pricefrom = document.getElementById('pricefrom').value;
         const priceto = document.getElementById('priceto').value;
@@ -33,6 +40,23 @@ function cleanform(event) {
         const returnacceptedvalue = document.getElementById('returnaccepted').checked;
         const freevalue = document.getElementById('free').checked;
         const expedictedvalue = document.getElementById('expedicted').checked;
+        const sortby = document.getElementById('sortby').value;
+
+        inputdatajson = {
+            'keyword': keywords,
+            'pricefrom': pricefrom,
+            'priceto': priceto,
+            'newvalue': newvalue,
+            'usedvalue':usedvalue,
+            'vergoodvalue': vergoodvalue,
+            'goodvalue': goodvalue,
+            'acceptablevalue': acceptablevalue,
+            'returnacceptedvalue': returnacceptedvalue,
+            'freevalue': freevalue,
+            'expedictedvalue': expedictedvalue,
+            'sortby': sortby
+        };
+        sendinputdata(inputdatajson);
     }
   }
 
@@ -61,13 +85,43 @@ function cleanform(event) {
         alert("Price Range values cannot be negative! Please try a value greater than or equal to 0.0");
         return false
     }
+    return true;
   }
 
-  function callFlaskFunction() {
-    fetch('/my-flask-function', {
-        method: 'GET',
+  function sendinputdata(data) {
+    keyword = data['keyword']
+    fetch('/sendinputdata', {
+        method: 'POST',
         headers: {
             'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data['data']['findItemsAdvancedResponse']); // Handle the response from Flask
+        const results = document.getElementById('total-results');
+        const result = document.createElement('div');
+        result.innerHTML = ''
+        result.className = "result"; // Add a CSS class
+        if((data['data']['findItemsAdvancedResponse'][0]['paginationOutput'] == undefined) || (data['data']['findItemsAdvancedResponse'][0]['paginationOutput'][0]['totalEntries'] == 0)) {
+            result.innerHTML = "No Results Found";
+            results.appendChild(result);
+            return false;
         }
+        else{
+            result.innerHTML = data['data']['findItemsAdvancedResponse'][0]['paginationOutput'][0]['totalEntries'] + " Results found for " + keyword;
+            results.appendChild(result);
+        }
+        
+
+        const itemscontainer = document.getElementById('items-container');
+        for (const itemcontent in data['data']['findItemsAdvancedResponse'][0]['searchResult'][0]['item']) {
+        const item = document.createElement('div');
+        item.className = "item"; // Add a CSS class
+        item.innerHTML = data['data']['findItemsAdvancedResponse'][0]['searchResult'][0]['item'][itemcontent]['title'][0]; // Add content
+        itemscontainer.appendChild(item);
+        // console.log(data['data']['findItemsAdvancedResponse'][0]['searchResult'][0]['item'][itemcontent]['title'][0])
+    };
     })
 }
