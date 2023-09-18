d1edf02cee591268799a03cc9b93153aff6fd33c
sakethanne <36626206+Sakethanne@users.noreply.github.com>
2023-09-18 01:07:30 -0700

    list of items and the corresponding links added

--- CSCI_571_Web_Tech/Assignment2/static/js/ebay.js
+++ CSCI_571_Web_Tech/Assignment2/static/js/ebay.js
@@ -19,6 +19,7 @@ function cleanform(event) {
     event.preventDefault(); // Prevent the default form submission behavior
     document.getElementById('total-results').innerHTML = '';
     document.getElementById('items-container').innerHTML = '';
+    document.getElementById('showmorebutton').innerHTML = '';
   }
 
   function submitdata(event) {
@@ -118,10 +119,23 @@ function cleanform(event) {
         counter = 0;
         const itemscontainer = document.getElementById('items-container');
         for (let counter = 0; counter < 3; counter++) {
-                const item = document.createElement('div');
-                item.className = "item"; // Add a CSS class
-                item.innerHTML = data['data']['findItemsAdvancedResponse'][0]['searchResult'][0]['item'][counter]['title'][0]; // Add content
-                itemscontainer.appendChild(item);
+            const item = document.createElement('button');
+            item.className = "item"; // Add a CSS class
+            htmltext = ""
+            htmltext = '<table class="individualitem"><tr class="individualitem"><td rowspan="4" class="individualitem" id="individualitem"><img src="' +data['data']['findItemsAdvancedResponse'][0]['searchResult'][0]['item'][counter]['galleryURL'][0]
+            htmltext += '" height="130px" width="130px"></td><th class="individualitem">' + data['data']['findItemsAdvancedResponse'][0]['searchResult'][0]['item'][counter]['title'][0]
+            htmltext += '</th></tr><tr class="individualitem"><td class="individualitem"> Category : ' + data['data']['findItemsAdvancedResponse'][0]['searchResult'][0]['item'][counter]['primaryCategory'][0]['categoryName'][0]
+            htmltext += '<a href="' + data['data']['findItemsAdvancedResponse'][0]['searchResult'][0]['item'][counter]['viewItemURL'][0]
+            htmltext += `"><img src="/static//images/redirect.png" height="18px" width="18px"></a>`
+            htmltext += '</td></tr><tr class="individualitem"><td class="individualitem"> Condition : ' + data['data']['findItemsAdvancedResponse'][0]['searchResult'][0]['item'][counter]['condition'][0]['conditionDisplayName'][0]
+            if(data['data']['findItemsAdvancedResponse'][0]['searchResult'][0]['item'][counter]['topRatedListing'][0].toString() != "false"){
+                htmltext += `<img src="/static//images/topRatedImage.png" height="25px" width="22px">`
+            }
+            htmltext += '</td></tr><tr class="individualitem"><th class="individualitem"> Price : $' + data['data']['findItemsAdvancedResponse'][0]['searchResult'][0]['item'][counter]['sellingStatus'][0]['currentPrice'][0]['__value__']
+            htmltext += '</th></tr></table>'
+            item.innerHTML = htmltext;
+            // console.log(htmltext)
+            itemscontainer.appendChild(item);
     };
     document.getElementById('showmorebutton').innerHTML = '<input class="showmore" id="showmoreless" type="button" value="Show More" onclick="getadditionaldata()">';
     })
@@ -131,9 +145,22 @@ function getadditionaldata() {
     counter = 3;
     const itemscontainer = document.getElementById('items-container');
     for (let counter = 3; counter < 10; counter++) {
-        const item = document.createElement('div');
+        const item = document.createElement('button');
         item.className = "item"; // Add a CSS class
-        item.innerHTML = jsonresponse['data']['findItemsAdvancedResponse'][0]['searchResult'][0]['item'][counter]['title'][0]; // Add content
+        htmltext = ""
+        htmltext = '<table class="individualitem"><tr class="individualitem"><td rowspan="4" class="individualitem" id="individualitem"><img src="' +jsonresponse['data']['findItemsAdvancedResponse'][0]['searchResult'][0]['item'][counter]['galleryURL'][0]
+        htmltext += '" height="130px" width="130px"></td><th class="individualitem">' + jsonresponse['data']['findItemsAdvancedResponse'][0]['searchResult'][0]['item'][counter]['title'][0]
+        htmltext += '</th></tr><tr class="individualitem"><td class="individualitem"> Category : ' + jsonresponse['data']['findItemsAdvancedResponse'][0]['searchResult'][0]['item'][counter]['primaryCategory'][0]['categoryName'][0]
+        htmltext += '<a href="' + jsonresponse['data']['findItemsAdvancedResponse'][0]['searchResult'][0]['item'][counter]['viewItemURL'][0]
+        htmltext += `"><img src="/static//images/redirect.png" height="18px" width="18px"></a>`
+        htmltext += '</td></tr><tr class="individualitem"><td class="individualitem"> Condition : ' + jsonresponse['data']['findItemsAdvancedResponse'][0]['searchResult'][0]['item'][counter]['condition'][0]['conditionDisplayName'][0]
+        if(jsonresponse['data']['findItemsAdvancedResponse'][0]['searchResult'][0]['item'][counter]['topRatedListing'][0].toString() != "false"){
+            htmltext += `<img src="/static//images/topRatedImage.png" height="25px" width="22px">`
+        }
+        htmltext += '</td></tr><tr class="individualitem"><th class="individualitem"> Price : $' + jsonresponse['data']['findItemsAdvancedResponse'][0]['searchResult'][0]['item'][counter]['sellingStatus'][0]['currentPrice'][0]['__value__']
+        htmltext += '</th></tr></table>'
+        item.innerHTML = htmltext;
+        // console.log(htmltext)
         itemscontainer.appendChild(item);
     };
     document.getElementById('showmorebutton').innerHTML = '<input class="showmore" id="showmoreless" type="button" value="Show Less" onclick="getolddata()">';
@@ -145,9 +172,22 @@ function getolddata() {
     const itemscontainer = document.getElementById('items-container');
     itemscontainer.innerHTML = '';
     for (let counter = 0; counter < 3; counter++) {
-        const item = document.createElement('div');
+        const item = document.createElement('button');
         item.className = "item"; // Add a CSS class
-        item.innerHTML = jsonresponse['data']['findItemsAdvancedResponse'][0]['searchResult'][0]['item'][counter]['title'][0]; // Add content
+        htmltext = ""
+        htmltext = '<table class="individualitem"><tr class="individualitem"><td rowspan="4" class="individualitem" id="individualitem"><img src="' +jsonresponse['data']['findItemsAdvancedResponse'][0]['searchResult'][0]['item'][counter]['galleryURL'][0]
+        htmltext += '" height="130px" width="130px"></td><th class="individualitem">' + jsonresponse['data']['findItemsAdvancedResponse'][0]['searchResult'][0]['item'][counter]['title'][0]
+        htmltext += '</th></tr><tr class="individualitem"><td class="individualitem"> Category : ' + jsonresponse['data']['findItemsAdvancedResponse'][0]['searchResult'][0]['item'][counter]['primaryCategory'][0]['categoryName'][0]
+        htmltext += '<a href="' + jsonresponse['data']['findItemsAdvancedResponse'][0]['searchResult'][0]['item'][counter]['viewItemURL'][0]
+        htmltext += `"><img src="/static//images/redirect.png" height="18px" width="18px"></a>`
+        htmltext += '</td></tr><tr class="individualitem"><td class="individualitem"> Condition : ' + jsonresponse['data']['findItemsAdvancedResponse'][0]['searchResult'][0]['item'][counter]['condition'][0]['conditionDisplayName'][0]
+        if(jsonresponse['data']['findItemsAdvancedResponse'][0]['searchResult'][0]['item'][counter]['topRatedListing'][0].toString() != "false"){
+            htmltext += `<img src="/static//images/topRatedImage.png" height="25px" width="22px">`
+        }
+        htmltext += '</td></tr><tr class="individualitem"><th class="individualitem"> Price : $' + jsonresponse['data']['findItemsAdvancedResponse'][0]['searchResult'][0]['item'][counter]['sellingStatus'][0]['currentPrice'][0]['__value__']
+        htmltext += '</th></tr></table>'
+        item.innerHTML = htmltext;
+        // console.log(htmltext)
         itemscontainer.appendChild(item);
     };
     document.getElementById('showmorebutton').innerHTML = '<input class="showmore" id="showmoreless" type="button" value="Show More" onclick="getadditionaldata()">';
