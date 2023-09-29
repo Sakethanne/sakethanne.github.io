554131b77b5ff898b0223b313180e2468892ae0a
sakethanne <36626206+Sakethanne@users.noreply.github.com>
2023-09-29 01:47:30 -0700

    GCP Commit

--- CSCI_571_Web_Tech/Assignment2/main.py
+++ CSCI_571_Web_Tech/Assignment2/main.py
@@ -13,7 +13,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('ebay.html')
+    return app.send_static_file('index.html')
+    # return render_template('index.html')
+
+if __name__ == '__main__':
+    #   app.run(debug=True)
+    # app.run(host='0.0.0.0', port=80)
+    app.run(debug=True, port=80)
 
 @app.route('/sendinputdata', methods=['GET'])
 def processinputdata():
