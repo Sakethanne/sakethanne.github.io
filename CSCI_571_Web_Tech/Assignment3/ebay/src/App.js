fec9e6cbacc99a37f1fadc744402edfad4156ed2
sakethanne <36626206+Sakethanne@users.noreply.github.com>
2023-10-23 18:31:17 -0700

    initial form

--- CSCI_571_Web_Tech/Assignment3/ebay/src/App.js
+++ CSCI_571_Web_Tech/Assignment3/ebay/src/App.js
@@ -1,46 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import React from "react"
+import ProductSearchForm from './ProductSearchForm';
+import FormExample from './text.jsx';
 
-// class App extends React.Component {
-//   state = {
-//     name: ""
-//   }
-
-//   componentDidMount() {
-//     fetch("http://localhost:3000")
-//       .then(res => res.json())
-//       .then(data => this.setState({ name: data.name }))
-//       // console.log(res);
-//   }
-
-//   render() {
-//     return (
-//       <h1>Hello {this.state.name}!</h1>
-//     )
-//   }
-// }
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div>
-      <h1>Saketh</h1>
-    </div>
-    </div>
+    <ProductSearchForm />
+    // <FormExample ></FormExample>
   );
 }
 
